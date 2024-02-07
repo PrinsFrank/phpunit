283b04a4046191ca72417ad69b0609e85f16acca
Sebastian Bergmann <sb@sebastian-bergmann.de>
2024-02-07 15:48:25 +0100

    Closes #5316

--- src/Framework/TestCase.php
+++ src/Framework/TestCase.php
@@ -1418,7 +1418,6 @@ abstract class TestCase extends Assert implements Reorderable, SelfDescribing, T
         $mockBuilder = $this->getMockBuilder($originalClassName)
             ->disableOriginalConstructor()
             ->disableOriginalClone()
-            ->disableArgumentCloning()
             ->onlyMethods($methods);
 
         if (!self::generateReturnValuesForTestDoubles()) {
